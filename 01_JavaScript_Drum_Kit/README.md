853308b0dbcbacf8852eb418dc6c39808e57c97c
ssommda <ekthadl13@gmail.com>
2019-05-21 22:48:21 +0900

    [DAY1] 기능 수정

--- 01_JavaScript_Drum_Kit/README.md
+++ 01_JavaScript_Drum_Kit/README.md
@@ -2,14 +2,13 @@
 
 ## 분석
 * 키보드의 A, S, D, F, G, H, J, K, L 키를 누르면 해당하는 버튼에 css 애니메이션을 동작시키고 해당 사운드 파일이 재생되어야 한다.
-* 기본 HTML 파일의 각 data-key에 있는 keyCode가 실제 keyCode와 다르다.
+* 기본 HTML 파일의 각 data-key에 keyCode는 'keydown' 이벤트의 keyCode와 일치한다.
 * 기본 CSS 파일에 `.playing` 이라는 클래스에 스타일과 애니메이션이 정의되어 있다.
 
 
 ## 설계
-1. 'data-key' 속성의 값을 실제 알파벳 key의 keyCode와 동일하게 수정해준다.
-2. 입력된 keyCode와 같은 숫자를 가지고 있는 버튼에 'playing' 클래스네임을 추가하고 애니메이션 시간이 끝날 때 다시 삭제한다.
-3. 입력된 keyCode와 같은 data-key 속성을 가지고 있는 audio를 play 한다.
+1. 입력된 keyCode와 같은 숫자를 가지고 있는 버튼에 'playing' 클래스네임을 추가하고 애니메이션 시간이 끝날 때 다시 삭제한다.
+2. 입력된 keyCode와 같은 data-key 속성을 가지고 있는 audio를 play 한다.
 
 ## 개선방안
 * 절차형이 아닌 함수형 프로그래밍으로 리팩토링
--- 01_JavaScript_Drum_Kit/script.js
+++ 01_JavaScript_Drum_Kit/script.js
@@ -6,39 +6,22 @@ const addClassPlaying = element => {
     );
 };
 
-const findAudioSrc = keyCode => {
-    const audios = document.getElementsByTagName('audio');
-    for(let i = 0; i < audios.length; i++) {
-        const audiosKey = audios[i].getAttribute('data-key');
-        if(Number(audiosKey) === keyCode) {
-            const audioSrc = audios[i].getAttribute('src');
-            return audioSrc
-        }
-    }
-};
-
-const findButton = keyCode => {
-    const buttons = document.getElementsByClassName('key');
-    for(let i = 0; i < buttons.length; i++) {
-        const buttonKey = buttons[i].getAttribute('data-key');
-        if(Number(buttonKey) === keyCode) {
-            addClassPlaying(buttons[i]);
-        }
-    }
-};
-
 const playMusic = keyCode => {
-    const targetSrc = findAudioSrc(keyCode);
-    const audio = new Audio(targetSrc);
+    const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+    const button = document.querySelector(`div[data-key='${keyCode}']`);
+    if(!button) {
+        return false;
+    }
+
+    addClassPlaying(button);
     audio.play();
 };
 
 
 const handleKeyPress = e => {
     const keyCode = e.keyCode;
-    findButton(keyCode);
     playMusic(keyCode);
 };
 
-document.addEventListener('keypress', handleKeyPress);
+window.addEventListener('keydown', handleKeyPress);
 
